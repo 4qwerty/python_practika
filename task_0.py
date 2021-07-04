8ce3c062fad0ed3b25e4399d32a0aadb641792ae
JOJO\38097 <kartemoko@gmail.com>
2021-07-04 16:35:33 +0300

    task_0

--- task_0.py
+++ task_0.py
@@ -1,8 +1,18 @@
 import random
+
+# генерування списку з 30 чисел з -100 до 100   
 arrayRand = random.sample(range(-100, 100), 30)
 print("\n")
+
 print("Сформувати список з 30 випадкових чисел:",arrayRand)
-print("\nМаксимальний елемент списку: ", max(arrayRand), "\nПорядковий номер: ", arrayRand.index(max(arrayRand)))
+
+# Максимальний елемент списку
+print("\nМаксимальний елемент списку: ") 
+print(max(arrayRand)) 
+# Порядковий номер максимального елементу списку
+print("Порядковий номер: ") 
+print(arrayRand.index(max(arrayRand)))
+
 listForSort = [i for i in arrayRand if (i % 2) == 1]
 if len(listForSort) == 0: 
     print("Непарних чисел не знайдено") 
